466bed133432db6191aa71bf58dddb0a30dff1a7
Sithija Shehara <175077u@uom.lk>
2019-08-14 07:12:10 -0700

    basic structure with drawer navigator.version 0.60

--- front/swg/App.js
+++ front/swg/App.js
@@ -1,28 +1,113 @@
-import React, { Component } from 'react';
-import {createStackNavigator, createAppContainer, createDrawerNavigator} from 'react-navigation';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
 
-import HomeScreen from './components/screens/Home';
-import CategoryScreen from './components/screens/Category';
-import WelcomeScreen from './components/screens/Welcome';
+import React, {Fragment} from 'react';
+import {createAppContainer, createDrawerNavigator} from 'react-navigation'
 
-export default class App extends Component {
-  render() {
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+class HomeScreen extends React.Component {
+    render() {
     return (
-      <AppContainer/>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello, world!</Text>
+        <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+          <Text>Open Drawer</Text>
+        </TouchableOpacity>
+      </View>
     );
   }
 }
 
-const AppDrawerNavigator = createDrawerNavigator({
-  Home: {screen: HomeScreen},
-  Category:{screen: CategoryScreen}
+class SettingsScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+          <Text>Open Drawer</Text>
+        </TouchableOpacity>
+        <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Settings</Text>
+      </View>
+    );
+  }
+}
 
+const DrawerNavigator = createDrawerNavigator(
+  {
+    Home: HomeScreen,
+    Settings: SettingsScreen,
+  },
+  {
+    hideStatusBar: true,
+    drawerBackgroundColor: 'rgba(255,255,255,.9)',
+    //overlayColor: '#ffff',
+    contentOptions: {
+      activeTintColor: '#fff',
+      activeBackgroundColor: '#EB3B5A',
+    },
+  }
+);
+
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
 });
 
-const AppStackNavigator = createStackNavigator({
-  Welcome:{screen: WelcomeScreen},
-  Home: {screen: AppDrawerNavigator},
 
-});
-
-const AppContainer = createAppContainer(AppStackNavigator);
+export default createAppContainer(DrawerNavigator);
