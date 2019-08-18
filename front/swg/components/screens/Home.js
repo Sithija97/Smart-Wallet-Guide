616c621ffc393d89b17297a93eec5e464120d233
Sithija Shehara <175077u@uom.lk>
2019-08-18 16:21:25 +0530

    tested vector icons for drawer nav icon

--- front/swg/components/screens/Home.js
+++ front/swg/components/screens/Home.js
@@ -1,10 +1,14 @@
 import React, {Fragment} from 'react';
 import { StyleSheet,View,Text,TouchableOpacity,} from 'react-native';
 
+//for vector icon
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export default class HomeScreen extends React.Component {
     render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Icon name="md-menu" size={30}/>
         <Text>Home</Text>
         <TouchableOpacity onPress={this.props.navigation.openDrawer}>
           <Text>Open Drawer</Text>
