9497289d7dea8f64ba0f2db55255662cd699a25d
Sithija Shehara <nsithijashehara@gmail.com>
2019-08-21 23:27:22 +0530

    added calander feature

--- front/swg/components/screens/Calander.js
+++ front/swg/components/screens/Calander.js
@@ -1,16 +1,52 @@
 import React, {Fragment} from 'react';
 import { StyleSheet,View,Text,TouchableOpacity,} from 'react-native';
 
-export default class CalanderScreen extends React.Component {
-    render() {
-      return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <TouchableOpacity onPress={this.props.navigation.openDrawer}>
-            <Text>Open Drawer</Text>
-          </TouchableOpacity>
-          <Text style={{ fontWeight: 'bold', marginTop: 20 }}>Calander</Text>
-        </View>
-      );
-    }
+import Calendar from 'react-native-calendario';
+export default class CalanderScreen extends React.Component{
+  render(){
+    return(
+      <View>
+        <Text style={{alignItems:'center', fontWeight:'bold', justifyContent:"center"}}>Calander</Text>
+        <TouchableOpacity onPress={this.props.navigation.openDrawer}>
+          <Text>Open Drawer</Text>
+        </TouchableOpacity>
+        <Calendar
+          onChange={range => console.log(range)}
+          minDate="2018-04-20"
+          startDate="2018-04-30"
+          endDate="2018-05-05"
+          theme={{
+            weekColumnTextStyle: {
+              color: 'red',
+            },
+            weekColumnStyle: {
+              paddingVertical: 20,
+            },
+            weekColumnsContainerStyle: {
+              backgroundColor: 'lightgrey',
+            },
+            monthTitleStyle: {
+              color: 'blue',
+            },
+            nonTouchableDayContainerStyle: {
+              backgroundColor: 'red',
+            },
+            nonTouchableDayTextStyle: {
+              color: 'green',
+            },
+            dayTextStyle: {
+              color: 'blue',
+            },
+            activeDayContainerStyle: {
+              backgroundColor: 'lightgrey',
+            },
+            activeDayTextStyle: {
+              color: 'red',
+            },
+          }}
+        />
+              </View>
+    );
   }
+}
   
